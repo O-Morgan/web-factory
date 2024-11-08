a389efd931ecb80ed66c956640cf92017920a7a2
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2024-11-08 23:29:16 +0000

    #7 added terraform provider

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,12 @@
-terraform 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.74.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"  # Specify your desired AWS region here
+}
