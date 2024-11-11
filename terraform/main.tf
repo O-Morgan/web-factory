c48bfaa8076683fd994f90273cb84b27deb3c65e
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2024-11-11 20:40:34 +0000

    #47 main tf formatted

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 # /workspaces/web-factory/terraform/main.tf
 
 module "networking" {
-  source = "./networking"  # Relative path from main.tf within the terraform directory
+  source = "./networking" # Relative path from main.tf within the terraform directory
   region = var.region
 }
 
