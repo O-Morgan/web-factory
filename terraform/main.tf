fcb5581d7690e3baed500c2de87a3a0885e4c520
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2024-11-12 18:19:49 +0000

    #51 test terraform (#52)
    * #51 test terraform
    
    * #52 fmt terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
-  ami           = "ami-0e8d228ad90af673b"  # Replace with a region-specific AMI ID
+  ami           = "ami-0e8d228ad90af673b" # Replace with a region-specific AMI ID
   instance_type = "t2.micro"
-  
+
   tags = {
     Name = "SimpleEC2Instance"
   }
