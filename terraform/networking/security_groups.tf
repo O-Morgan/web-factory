29998af145cbab7d3f1678179d91ce8d867a8caf
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2024-11-13 15:15:34 +0000

    #73 syntax fix to sg

--- terraform/networking/security_groups.tf
+++ terraform/networking/security_groups.tf
@@ -1,24 +1,31 @@
 resource "aws_security_group" "wf_alb_sg" {
   description = "Security group for Application Load Balancer"
   vpc_id      = aws_vpc.wf_main_vpc.id
+
   ingress {
     from_port   = var.http_port
     to_port     = var.http_port
     protocol    = "tcp"
     cidr_blocks = [var.allowed_http_cidr]
+    description = "Allow HTTP traffic from allowed CIDR"
   }
+
   ingress {
     from_port   = var.https_port
     to_port     = var.https_port
     protocol    = "tcp"
     cidr_blocks = [var.allowed_https_cidr]
+    description = "Allow HTTPS traffic from allowed CIDR"
   }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
   }
+
   tags = {
     Name = "WF_ALB_SG"
   }
@@ -26,20 +33,26 @@ resource "aws_security_group" "wf_alb_sg" {
 
 resource "aws_security_group" "wf_web_sg" {
   description = "Security group for web servers in private subnets"
-  vpc_id = aws_vpc.wf_main_vpc.id
+  vpc_id      = aws_vpc.wf_main_vpc.id
+
   ingress {
     from_port       = var.web_server_port
     to_port         = var.web_server_port
     protocol        = "tcp"
     security_groups = [aws_security_group.wf_alb_sg.id]
+    description     = "Allow traffic from ALB security group"
   }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
   }
+
   tags = {
     Name = "WF_Web_SG"
   }
 }
+
