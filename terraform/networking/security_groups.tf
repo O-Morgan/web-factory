4607135b801781023722a63874ce79329cc3bb9b
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2024-11-13 14:19:36 +0000

    #67 updated the prefix of wf on all resources (#68)

--- terraform/networking/security_groups.tf
+++ terraform/networking/security_groups.tf
@@ -1,57 +1,43 @@
-# Security Group for ALB
-resource "aws_security_group" "alb_sg" {
-  vpc_id = aws_vpc.main_vpc.id
-
+resource "aws_security_group" "wf_alb_sg" {
+  vpc_id = aws_vpc.wf_main_vpc.id
   ingress {
     from_port   = var.http_port
     to_port     = var.http_port
     protocol    = "tcp"
     cidr_blocks = [var.allowed_http_cidr]
-    description = "Allow HTTP traffic from specified CIDR range"
   }
-
   ingress {
     from_port   = var.https_port
     to_port     = var.https_port
     protocol    = "tcp"
     cidr_blocks = [var.allowed_https_cidr]
-    description = "Allow HTTPS traffic from specified CIDR range"
   }
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow all outbound traffic"
   }
-
   tags = {
-    Name = "alb_sg"
+    Name = "WF_ALB_SG"
   }
 }
 
-# Security Group for Web Servers in Private Subnets
-resource "aws_security_group" "web_sg" {
-  vpc_id = aws_vpc.main_vpc.id
-
+resource "aws_security_group" "wf_web_sg" {
+  vpc_id = aws_vpc.wf_main_vpc.id
   ingress {
     from_port       = var.web_server_port
     to_port         = var.web_server_port
     protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
-    description     = "Allow traffic from ALB on web server port"
+    security_groups = [aws_security_group.wf_alb_sg.id]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow all outbound traffic"
   }
-
   tags = {
-    Name = "web_sg"
+    Name = "WF_Web_SG"
   }
 }
--- terraform/networking/vpc.tf
+++ terraform/networking/vpc.tf
@@ -1,71 +1,79 @@
-resource "aws_vpc" "main_vpc" {
+resource "aws_vpc" "wf_main_vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = "main_vpc"
+    Name = "WF_Main_VPC"
   }
 }
 
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id                  = aws_vpc.main_vpc.id
+resource "aws_subnet" "wf_public_subnet_1" {
+  vpc_id                  = aws_vpc.wf_main_vpc.id
   cidr_block              = var.public_subnet_cidrs[0]
   availability_zone       = var.availability_zones[0]
   map_public_ip_on_launch = true
   tags = {
-    Name = "public_subnet_1"
+    Name = "WF_Public_Subnet_1"
   }
 }
 
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id                  = aws_vpc.main_vpc.id
+resource "aws_subnet" "wf_public_subnet_2" {
+  vpc_id                  = aws_vpc.wf_main_vpc.id
   cidr_block              = var.public_subnet_cidrs[1]
   availability_zone       = var.availability_zones[1]
   map_public_ip_on_launch = true
   tags = {
-    Name = "public_subnet_2"
+    Name = "WF_Public_Subnet_2"
   }
 }
 
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id            = aws_vpc.main_vpc.id
+resource "aws_subnet" "wf_private_subnet_1" {
+  vpc_id            = aws_vpc.wf_main_vpc.id
   cidr_block        = var.private_subnet_cidrs[0]
   availability_zone = var.availability_zones[0]
   tags = {
-    Name = "private_subnet_1"
+    Name = "WF_Private_Subnet_1"
   }
 }
 
-resource "aws_subnet" "private_subnet_2" {
-  vpc_id            = aws_vpc.main_vpc.id
+resource "aws_subnet" "wf_private_subnet_2" {
+  vpc_id            = aws_vpc.wf_main_vpc.id
   cidr_block        = var.private_subnet_cidrs[1]
   availability_zone = var.availability_zones[1]
   tags = {
-    Name = "private_subnet_2"
+    Name = "WF_Private_Subnet_2"
   }
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main_vpc.id
+resource "aws_internet_gateway" "wf_igw" {
+  vpc_id = aws_vpc.wf_main_vpc.id
   tags = {
-    Name = "igw"
+    Name = "WF_Internet_Gateway"
   }
 }
 
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.main_vpc.id
+# Public Route Table
+resource "aws_route_table" "wf_public_route_table" {
+  vpc_id = aws_vpc.wf_main_vpc.id
+
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.wf_igw.id
+  }
+
+  tags = {
+    Name = "WF_Public_Route_Table"
   }
 }
 
-resource "aws_route_table_association" "public_subnet_assoc_1" {
-  subnet_id      = aws_subnet.public_subnet_1.id
-  route_table_id = aws_route_table.public_route_table.id
+# Route Table Association for Public Subnet 1
+resource "aws_route_table_association" "wf_public_subnet_assoc_1" {
+  subnet_id      = aws_subnet.wf_public_subnet_1.id
+  route_table_id = aws_route_table.wf_public_route_table.id
 }
 
-resource "aws_route_table_association" "public_subnet_assoc_2" {
-  subnet_id      = aws_subnet.public_subnet_2.id
-  route_table_id = aws_route_table.public_route_table.id
+# Route Table Association for Public Subnet 2
+resource "aws_route_table_association" "wf_public_subnet_assoc_2" {
+  subnet_id      = aws_subnet.wf_public_subnet_2.id
+  route_table_id = aws_route_table.wf_public_route_table.id
 }
