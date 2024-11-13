2575315027d8a07979a8e235c455dbc3d6e6bb0a
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2024-11-13 20:51:18 +0000

    #81 update and testing plan

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,8 +14,8 @@ module "networking" {
 module "compute" {
   source                = "./compute"
   vpc_id                = module.networking.wf_vpc_id
-  public_subnet_ids     = module.networking.wf_public_subnet_ids
-  private_subnet_ids    = module.networking.wf_private_subnet_ids
+  public_subnets        = module.networking.wf_public_subnets  # Matches variable name exactly
+  private_subnets       = module.networking.wf_private_subnets # Matches variable name exactly
   alb_security_group_id = module.networking.wf_alb_sg_id
   web_security_group_id = module.networking.wf_web_sg_id
 
@@ -28,6 +28,7 @@ module "compute" {
   hosted_zone_id     = var.hosted_zone_id
   certificate_arn    = var.certificate_arn
 }
+
 
 #key_name           = var.key_name
 #public_key         = var.public_key # Ensure public_key is passed to compute module
