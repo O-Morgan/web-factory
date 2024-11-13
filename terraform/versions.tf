b97292977b34aa456abb63a16d05749783ed385c
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2024-11-13 11:17:54 +0000

    #55 had to update version with correct bucket name

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = "~> 1.9.8"
   backend "s3" {
-    bucket         = "webfactory-tf-backend"
+    bucket         = "2345-webfactory-tf-backend-2345"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "WebFactoryTerraformLocks"
