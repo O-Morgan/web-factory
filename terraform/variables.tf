a5253dd3bae8f2a02ad41f2efaeb9783c1b3c87b
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2024-11-13 13:26:32 +0000

    #63 added networking module variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,56 @@ variable "region" {
   type        = string
   default     = "eu-west-2" # Set to your region of choice
 }
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "availability_zones" {
+  description = "Availability zones for subnets"
+  type        = list(string)
+  default     = ["eu-west-2a", "eu-west-2b"]
+}
+variable "http_port" {
+  description = "HTTP port for ALB"
+  type        = number
+  default     = 80
+}
+
+variable "https_port" {
+  description = "HTTPS port for ALB"
+  type        = number
+  default     = 443
+}
+
+variable "allowed_http_cidr" {
+  description = "CIDR block allowed for HTTP traffic"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "allowed_https_cidr" {
+  description = "CIDR block allowed for HTTPS traffic"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "web_server_port" {
+  description = "Port for web server traffic from ALB"
+  type        = number
+  default     = 80
+}
