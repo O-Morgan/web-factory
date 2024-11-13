54acf5777a30442ef8ce10e24cd1488efb5db9d8
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2024-11-13 11:18:57 +0000

    #55 updated region buckets in

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket         = "2345-webfactory-tf-backend-2345"
     key            = "terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-west-2"
     dynamodb_table = "WebFactoryTerraformLocks"
   }
 }
