9ea36c5d500757d195998d0554517cc9347538c1
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2024-11-13 11:22:22 +0000

    #55 updated and checked dynamodb

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = "~> 1.9.8"
   backend "s3" {
-    bucket         = "2345-webfactory-tf-backend-2345"
+    bucket         = "2345-webfactory-tf-backend-2345"  # Ensure this is the correct S3 bucket name
     key            = "terraform.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "WebFactoryTerraformLocks"
+    region         = "eu-west-2"  # Updated region to match your AWS setup
+    dynamodb_table = "WebfactoryTerraformLocks"  # Correct DynamoDB table name
   }
 }
