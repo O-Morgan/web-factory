a52072f46f459004713ae65ec055a99a50fc7b5e
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2024-11-13 11:23:56 +0000

    #55 fixed tf format issue

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "2345-webfactory-tf-backend-2345"  # Ensure this is the correct S3 bucket name
+    bucket         = "2345-webfactory-tf-backend-2345" # Ensure this is the correct S3 bucket name
     key            = "terraform.tfstate"
-    region         = "eu-west-2"  # Updated region to match your AWS setup
-    dynamodb_table = "WebfactoryTerraformLocks"  # Correct DynamoDB table name
+    region         = "eu-west-2"                # Updated region to match your AWS setup
+    dynamodb_table = "WebfactoryTerraformLocks" # Correct DynamoDB table name
   }
 }
