71b75471e901ec0926113457b902e1b05b08e330
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2024-11-13 20:36:37 +0000

    #81 minor adjustments to configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,8 +14,8 @@ module "networking" {
 module "compute" {
   source                = "./compute"
   vpc_id                = module.networking.wf_vpc_id
-  public_subnets        = module.networking.wf_public_subnet_ids  # Match expected name in compute
-  private_subnets       = module.networking.wf_private_subnet_ids # Match expected name in compute
+  public_subnets        = module.networking.wf_public_subnet_ids
+  private_subnets       = module.networking.wf_private_subnet_ids
   alb_security_group_id = module.networking.wf_alb_sg_id
   web_security_group_id = module.networking.wf_web_sg_id
 
@@ -28,7 +28,6 @@ module "compute" {
   hosted_zone_id     = var.hosted_zone_id
   certificate_arn    = var.certificate_arn
 }
-
 
 
 #key_name           = var.key_name
