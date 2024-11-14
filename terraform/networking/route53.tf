ac2bb90cda2439916819b783f5d5fc43437c5bc9
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2024-11-14 02:02:42 +0000

    #89 test tweak- re run

--- terraform/networking/route53.tf
+++ terraform/networking/route53.tf
@@ -1,6 +1,1 @@
-# Output for ACM Certificate ARN to use with ALB in compute module
-output "certificate_arn" {
-  description = "The ARN of the ACM certificate for use with ALB"
-  value       = var.certificate_arn
-}
 
