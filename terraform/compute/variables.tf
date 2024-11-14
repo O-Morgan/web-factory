b70a15e0f46f83a396c2945b98959840bb240575
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2024-11-14 23:53:01 +0000

    101 entered issues

--- terraform/compute/variables.tf
+++ terraform/compute/variables.tf
@@ -1,30 +1,5 @@
-# Networking references from networking module outputs
-variable "vpc_id" {
-  description = "VPC ID for EC2 instances"
-  type        = string
-}
+# compute/variables.tf
 
-variable "public_subnet_ids" {
-  description = "List of public subnet IDs for ALB"
-  type        = list(string)
-}
-
-variable "private_subnet_ids" {
-  description = "List of private subnet IDs for EC2 instances"
-  type        = list(string)
-}
-
-variable "alb_security_group_id" {
-  description = "Security group ID for ALB"
-  type        = string
-}
-
-variable "web_security_group_id" {
-  description = "Security group ID for web servers"
-  type        = string
-}
-
-# Compute-specific configurations
 variable "instance_type" {
   description = "Instance type for EC2 instances"
   type        = string
@@ -45,13 +20,6 @@ variable "max_instance_count" {
   type        = number
 }
 
-variable "certificate_arn" {
-  description = "Certificate ARN for SSL (if reusing an existing certificate)"
-  type        = string
-  default     = ""
-}
-# compute/variables.tf
-
 variable "domain_name" {
   description = "Primary domain name for Route 53 and ACM certificate"
   type        = string
@@ -59,5 +27,10 @@ variable "domain_name" {
 
 variable "hosted_zone_id" {
   description = "Hosted Zone ID for Route 53"
+  type        = string
+}
+
+variable "certificate_arn" {
+  description = "Certificate ARN for SSL (if reusing an existing certificate)"
   type        = string
 }
