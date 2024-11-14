34537a0f5a046dfe86a0648028d8b3922d731408
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2024-11-14 02:10:12 +0000

    #89 syntax error updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,8 +10,8 @@ module "networking" {
   allowed_https_cidr   = var.allowed_https_cidr
   web_server_port      = var.web_server_port
 
-  alb_dns_name    = module.compute.alb_dns_name
-  alb_zone_id     = module.compute.alb_zone_id
+  alb_dns_name = module.compute.alb_dns_name
+  alb_zone_id  = module.compute.alb_zone_id
 }
 
 module "compute" {
@@ -22,7 +22,6 @@ module "compute" {
   alb_security_group_id = module.networking.wf_alb_sg_id
   web_security_group_id = module.networking.wf_web_sg_id
   certificate_arn       = module.compute.certificate_arn
-}
 
   # Additional compute-specific variables
   instance_type      = var.instance_type
@@ -31,5 +30,6 @@ module "compute" {
   max_instance_count = var.max_instance_count
   domain_name        = var.domain_name
   hosted_zone_id     = var.hosted_zone_id
-  certificate_arn    = var.certificate_arn
+  #certificate_arn    = var.certificate_arn
+
 }
