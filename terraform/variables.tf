0533510e8cd3e72fa13986452064534d164f188b
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2024-11-15 19:59:29 +0000

    #109 added context to root vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -22,33 +22,39 @@ variable "certificate_arn" {
   type        = string
 }
 variable "vpc_cidr" {
-  description = "The CIDR block for the VPC"
+  description = "CIDR block for the VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
   description = "List of CIDR blocks for public subnets"
   type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   description = "List of CIDR blocks for private subnets"
   type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "availability_zones" {
   description = "Availability zones for subnets"
   type        = list(string)
+  default     = ["eu-west-2a", "eu-west-2b"]
 }
 
 variable "http_port" {
   description = "HTTP port for ALB"
   type        = number
+  default     = 80
 }
 
 variable "https_port" {
   description = "HTTPS port for ALB"
   type        = number
+  default     = 443
 }
 
 variable "allowed_http_cidr" {
@@ -66,6 +72,7 @@ variable "allowed_https_cidr" {
 variable "web_server_port" {
   description = "Port for web server traffic from ALB"
   type        = number
+  default     = 8080
 }
 
 variable "instance_type" {
