1731a38c5cd889573f52d1d57d63111412e5ecfa
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2024-11-15 20:53:54 +0000

    109 duplicate output deleted

--- terraform/compute/loadbalancer.tf
+++ terraform/compute/loadbalancer.tf
@@ -54,12 +54,3 @@ resource "aws_lb_listener" "http_listener" {
   }
 }
 
-output "alb_dns_name" {
-  description = "DNS name of the Application Load Balancer"
-  value       = aws_lb.wf_alb.dns_name
-}
-
-output "alb_zone_id" {
-  description = "Zone ID of the Application Load Balancer"
-  value       = aws_lb.wf_alb.zone_id
-}
