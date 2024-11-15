4b5269a798f38bfd16c33925c8a1ded9e4fe0f0e
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2024-11-15 21:19:16 +0000

    #109 had to add a variable to networking module

--- terraform/networking/variables.tf
+++ terraform/networking/variables.tf
@@ -57,3 +57,13 @@ variable "alb_dns_name" {
   description = "DNS name of the ALB"
   type        = string
 }
+
+variable "alb_dns_name" {
+  description = "DNS name of the ALB"
+  type        = string
+}
+
+variable "alb_zone_id" {
+  description = "Zone ID of the ALB"
+  type        = string
+}
