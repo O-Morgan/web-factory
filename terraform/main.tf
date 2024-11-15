c48f7208876deca0ca2ef92e3d4c010584b2ca68
Owen Morgan <96142367+O-Morgan@users.noreply.github.com>
2024-11-15 19:26:20 +0000

    #109 testing changes to root var

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,10 +9,7 @@ module "networking" {
   allowed_http_cidr    = var.allowed_http_cidr
   allowed_https_cidr   = var.allowed_https_cidr
   web_server_port      = var.web_server_port
-  domain_name          = var.domain_name
-  hosted_zone_id       = var.hosted_zone_id
 }
-
 
 module "compute" {
   source                = "./compute"
@@ -22,12 +19,10 @@ module "compute" {
   alb_security_group_id = module.networking.wf_alb_sg_id
   web_security_group_id = module.networking.wf_web_sg_id
 
-  certificate_arn = var.certificate_arn
-
+  certificate_arn    = var.certificate_arn
   instance_type      = var.instance_type
   ami_id             = var.ami_id
   min_instance_count = var.min_instance_count
   max_instance_count = var.max_instance_count
 }
-
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,3 +21,67 @@ variable "certificate_arn" {
   description = "Certificate ARN for SSL"
   type        = string
 }
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "Availability zones for subnets"
+  type        = list(string)
+}
+
+variable "http_port" {
+  description = "HTTP port for ALB"
+  type        = number
+}
+
+variable "https_port" {
+  description = "HTTPS port for ALB"
+  type        = number
+}
+
+variable "allowed_http_cidr" {
+  description = "CIDR block allowed for HTTP traffic"
+  type        = string
+}
+
+variable "allowed_https_cidr" {
+  description = "CIDR block allowed for HTTPS traffic"
+  type        = string
+}
+
+variable "web_server_port" {
+  description = "Port for web server traffic from ALB"
+  type        = number
+}
+
+variable "instance_type" {
+  description = "Instance type for EC2 instances"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+}
+
+variable "min_instance_count" {
+  description = "Minimum number of instances in the Auto Scaling group"
+  type        = number
+}
+
+variable "max_instance_count" {
+  description = "Maximum number of instances in the Auto Scaling group"
+  type        = number
+}
