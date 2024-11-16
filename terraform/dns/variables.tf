735d594999866b3fe526371d0a1bb1a52210a53a
Owen Morgan <owen.morgan7300@gmail.com>
2024-11-16 00:40:03 +0000

    # update vars with alb arguments removed

--- terraform/dns/variables.tf
+++ terraform/dns/variables.tf
@@ -8,13 +8,3 @@ variable "hosted_zone_id" {
   description = "Hosted Zone ID in Route 53"
   type        = string
 }
-
-#variable "alb_dns_name" {
-#  description = "The DNS name of the ALB"
-#  type        = string
-#}
-
-#variable "alb_zone_id" {
-# description = "The Zone ID of the ALB"
-#  type        = string
-#}
